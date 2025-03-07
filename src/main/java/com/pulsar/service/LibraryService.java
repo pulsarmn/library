2da2c037ce8c974c9eb13878c6a0c7b1edaa7292
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-07 20:57:14 +0300

    add: create the LibraryService class

---
+++ src/main/java/com/pulsar/service/LibraryService.java
@@ -0,0 +1,16 @@
+package com.pulsar.service;
+
+import com.pulsar.Catalog;
+
+public class LibraryService {
+
+    private final Catalog catalog;
+
+    public LibraryService() {
+        this(new Catalog());
+    }
+
+    public LibraryService(Catalog catalog) {
+        this.catalog = catalog;
+    }
+}
