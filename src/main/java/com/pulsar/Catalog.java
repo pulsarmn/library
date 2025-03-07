9413ecd3e4680a52457f1f658db369df4770b190
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-07 20:01:09 +0300

    add: implement the print() method in the Catalog class

--- src/main/java/com/pulsar/Catalog.java
+++ src/main/java/com/pulsar/Catalog.java
@@ -1,6 +1,7 @@
 package com.pulsar;
 
 import com.pulsar.model.LibraryItem;
+import com.pulsar.util.Printer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +16,14 @@ public class Catalog {
 
     public Catalog(List<LibraryItem> catalog) {
         this.catalog = catalog;
+    }
+
+    public void print() {
+        if (catalog.isEmpty()) {
+            Printer.error("Каталог пуст!");
+        }else {
+            Printer.success("Каталог:");
+            catalog.forEach(System.out::println);
+        }
     }
 }
