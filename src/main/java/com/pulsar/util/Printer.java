08ebc869e752d2c712621e971c429672511f6ae3
Кузичев Александр <pulsarmn@yandex.ru>
2025-03-07 19:49:59 +0300

    add: implement 6 methods in the Printer class
    methods:
    - inputRequest()
    - inputError()
    - error()
    - success()
    - print()
    - println()

--- src/main/java/com/pulsar/util/Printer.java
+++ src/main/java/com/pulsar/util/Printer.java
@@ -6,7 +6,8 @@ public final class Printer {
     private static final String BOLD_GREEN = "\033[1;32m";
     private static final String RESET = "\033[0m";
 
-    private Printer() {}
+    private Printer() {
+    }
 
     public static void mainMenu() {
         System.out.println("1. Посмотреть каталог");
@@ -14,5 +15,30 @@ public final class Printer {
         System.out.println("3. Выдать объект");
         System.out.println("4. Вернуть объект");
         System.out.println("0. Выйти");
+    }
+
+    public static void inputRequest() {
+        System.out.print("> ");
+    }
+
+    public static void inputError() {
+        String message = "Некорректный ввод! Попробуйте ещё раз!";
+        error(message);
+    }
+
+    public static void error(String message) {
+        System.out.println(BOLD_RED + message + RESET);
+    }
+
+    public static void success(String message) {
+        System.out.println(BOLD_GREEN + message + RESET);
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static void println(String message) {
+        System.out.println(message);
     }
 }
